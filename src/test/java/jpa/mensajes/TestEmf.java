e8a8d6514132a88aa6912fdc11d018696cae57d2
Juan Ortiz Márquez <juanortizmarquez005@gmail.com>
2021-09-01 12:03:51 +0200

    Initial commit

---
+++ src/test/java/jpa/mensajes/TestEmf.java
@@ -0,0 +1,19 @@
+package jpa.mensajes;
+
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import static org.junit.Assert.*;
+
+public class TestEmf {
+    @Test
+    public void createEntityManagerTest() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mensajes-mysql");
+        EntityManager em = emf.createEntityManager();
+        assertNotNull(em);
+        em.close();
+    }
+}
